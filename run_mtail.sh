e6db58d458625304d579a1d15985811e2d1b9248
objectc <tab88tab@gmail.com>
2021-05-13 23:16:31 -0400

    update chia location

--- run_mtail.sh
+++ run_mtail.sh
@@ -1,2 +1,2 @@
-nohup watch -n 30 "~/chia-blockchain/venv/bin/chia farm summary > ~/.chia/mainnet/log/farmer_summary.log" &
+nohup watch -n 30 "~/miniconda3/envs/plot/bin/chia farm summary > ~/.chia/mainnet/log/farmer_summary.log" &
 nohup ./mtail --progs ./ --logs ~/.chia/mainnet/log/debug.log --logs ~/.chia/mainnet/log/farmer_summary.log --poll_interval 3000ms &
