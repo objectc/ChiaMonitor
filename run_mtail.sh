b5a15aa977466a1c85dbd3d07ab8242cf4814fc4
objectc <tab88tab@gmail.com>
2021-05-28 15:20:10 -0400

    Update run_mtail.sh

--- run_mtail.sh
+++ run_mtail.sh
@@ -1,2 +1,2 @@
 nohup watch -n 30 "~/miniconda3/envs/plot/bin/chia farm summary > ~/.chia/mainnet/log/farmer_summary.log" &
-nohup ./mtail --progs ./ --logs ~/.chia/mainnet/log/debug.log --logs ~/.chia/mainnet/log/farmer_summary.log --poll_interval 3000ms &
+nohup ./mtail --progs ./ --logs ~/.chia/mainnet/log/debug.log --logs ~/.chia/mainnet/log/farmer_summary.log --poll_interval 15000ms &
